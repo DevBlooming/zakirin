fa6526e9aa893319e980e7c398d34434dd0c5f7f
DevBlooming <131426155+DevBlooming@users.noreply.github.com>
2023-12-21 06:07:51 +0200

    fix app name

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,5 +1,6 @@
 class Constants {
-  static const String appName = "Zakirin";
+  static const String appName = "Zakirin | ذَّاكِرِينَ";
+  static const String appNameEnglish = "Zakirin";
   static const String appNameArabic = "ذَّاكِرِينَ";
   static const String azkarSectionTitle = "الأذكار";
 }
